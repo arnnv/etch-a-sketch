1f2e0b6d410e0cf4db386677c8b6fd983a669003
Arnav Gupta <109935951+arnnv@users.noreply.github.com>
2023-08-15 11:18:03 +0530

    Add button for changing grid size

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 const grid = document.querySelector(".grid");
-
 const gridStyle = getComputedStyle(grid);
-
 const gridWidthPx = parseInt(gridStyle.width);
 const gridHeightPx = parseInt(gridStyle.height);
+const changeSizeBtn = document.querySelector(".change-size");
 
-let n = 16;
+function createBoxes(qty) {
+  for (let i = 1; i <= qty; i++) {
+    for (let j = 1; j <= qty; j++) {
+      let box = document.createElement("div");
+      box.classList.add("box");
+      box.style.width = `${gridWidthPx / qty}px`;
+      box.style.height = `${gridHeightPx / qty}px`;
+      grid.appendChild(box);
+    }
+  }
 
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= n; j++) {
-    let box = document.createElement("div");
-    box.classList.add("box");
-    box.style.width = `${gridWidthPx / n}px`;
-    box.style.height = `${gridHeightPx / n}px`;
-    grid.appendChild(box);
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      box.style.backgroundColor = `#${randomColor}`;
+    });
+  });
+}
+
+createBoxes(16);
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.lastChild);
   }
 }
 
-const boxes = document.querySelectorAll(".box");
+function changeGridSize(newSize) {
+  removeAllChildNodes(grid);
+  createBoxes(newSize);
+}
 
-boxes.forEach((box) => {
-  box.addEventListener("mouseover", () => {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    box.style.backgroundColor = `#${randomColor}`;
-  });
-});
+function changeSizeBtnFn() {
+  let newSize = parseInt(prompt("Enter new size: ", 16));
+  if (newSize >= 1 && newSize <= 100) {
+    changeGridSize(newSize);
+  } else if (newSize <= 0 || newSize > 100) {
+    if (confirm("Enter a size between 1 & 100.")) {
+      changeSizeBtnFn();
+    }
+  }
+}
+
+changeSizeBtn.addEventListener("click", changeSizeBtnFn);
